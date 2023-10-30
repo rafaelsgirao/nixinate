59058ef88822681f9f1703a4588286e996582032
Rafael Girão <git@rafael.ovh>
2023-10-30 18:16:51 +0000

    Add all nixos-rebuild actions

--- nixinate/generate-apps.nix
+++ nixinate/generate-apps.nix
@@ -7,20 +7,25 @@ let
       (x: nixpkgs.lib.optionalString
         (flake.nixosConfigurations."${x}".config.deploy.enable) "${x}"));
   mkDeployScript = import ./make-deploy-script.nix { inherit nixpkgs pkgs flake; };
+  #All main arguments to nixos-rebuild (taken from manpage), except 'edit.
+  rebuildActions = [ "switch" "boot" "test" "build" "dry-build" "dry-activate" "build-vm" "build-vm-with-bootloader" ];
 in
-nixpkgs.lib.genAttrs
-  validMachines
-  (x:
-    {
-      type = "app";
-      program = toString (mkDeployScript {
-        machine = x;
-        dryRun = false;
-      });
-    }
-  )
-  // nixpkgs.lib.genAttrs
-  (map (a: a + "-dry-run") validMachines)
+# nixpkgs.lib.genAttrs
+#   validMachines
+#   (x:
+#     {
+#       type = "app";
+#       program = toString (mkDeployScript {
+#         machine = x;
+#         dryRun = false;
+#       });
+#     }
+#   )
+  # // nixpkgs.lib.genAttrs
+  # (map (a: a + "-dry-run") validMachines)
+   nixpkgs.lib.genAttrs
+   (map (rebuildAction: (map: machineName: (machineName + "-" + rebuildAction) validMachines) ) rebuildActions )
+  # (map (a: a + "-dry-run") validMachines)
   (x:
     {
       type = "app";
